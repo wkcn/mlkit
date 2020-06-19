15b56bfa636368846ba134d443c8fdb360105839
wkcn <wkcn@live.cn>
2020-06-19 17:51:18 +0800

    fix threshold

--- boosted_tree/cpp/xgboost_demo.py
+++ boosted_tree/cpp/xgboost_demo.py
@@ -5,7 +5,7 @@ def compute_acc(a, b):
     assert a.ndim == 1
     if a.shape != b.shape:
         return 0
-    return np.count_nonzero((a > 0.5) & (b > 0.5)) / len(a)
+    return np.count_nonzero((a >= 0.5) & (b >= 0.5)) / len(a)
 
 # read in data
 dtrain = xgb.DMatrix('./data/agaricus.txt.train')
@@ -15,8 +15,8 @@ dtest = xgb.DMatrix('./data/agaricus.txt.test')
 param = {'max_depth':2, 'eta':1, 'objective':'binary:logistic' }
 num_round = 2
 bst = xgb.train(param, dtrain, num_round)
-# make prediction
 
+# make prediction
 train_preds = bst.predict(dtrain)
 train_acc = compute_acc(train_preds, dtrain.get_label())
 print(f"Training Accuracy: {train_acc}")
